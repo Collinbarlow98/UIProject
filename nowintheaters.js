d5b919f69e35955cc2aec209a2b3e05b22707e3f
Collin Barlow <Collin@Collins-MacBook-Pro.local>
2019-12-11 11:27:59 -0600

    update

--- nowintheaters.js
+++ nowintheaters.js
@@ -1,13 +1,13 @@
-fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=ddcb97784f13102b72af6aa3e89493b4")
-.then((response) => {
-  return response.json()
-})
-.then(json => {
-console.log(json)
-let results = json.results
-let nowplaying = results.map((result) => {
-  console.log(result)
-  return `<li>${result.original_title}</li>`
-})
-moviesUL.innerHTML = nowplaying.join('')
-})
+fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&page=1&region=US")
+  .then((response) => {
+    return response.json()
+  })
+  .then(json => {
+    let results = json.results
+    let nowplaying = results.map((result) => {
+      let poster = result.poster_path
+      return `<div><img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" style="height: 250; width: 200"/> <li>${result.title}</li></div>`
+
+    })
+    moviesUL.innerHTML += nowplaying.join('')
+  })
--- reviews.js
+++ reviews.js
@@ -10,38 +10,24 @@ moviesearch.addEventListener('click', () => {
     })
     .then(json => {
       let results = json.results
-      let nowplaying = results.map((result1) => {
-        let id = result1.id
-        let movietitle = `<li>${result1.original_title}</li>`
-
-        fetch(`https://api.themoviedb.org/3/movie/${id}/images?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en`)
-          .then((response) => {
-            return response.json()
-          })
-          .then(json => {
-            let results = json.posters
-            let filepath = results[0]
-
-            return poster = `<img src="https://image.tmdb.org/t/p/w500/${filepath.file_path}" style="height: 250; width: 200"/>`
-        })
+      let nowplaying = results.map((result) => {
+        let id = result.id
 
         fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&page=1`)
           .then((response) => {
             return response.json()
           })
           .then(json => {
-            let results = json.results
-            let nowplaying = results.map((result) => {
-              return `<li>${result.content}</li>`
-            })
-            if(json.results != 0) {
-              let div = document.createElement("DIV")
-              div.innerHTML += poster
-              div.innerHTML += movietitle
-              div.innerHTML += nowplaying.join('')
-              document.body.append(div);
+            let reviewArray = json.results
+            if(reviewArray != 0) {
+              let reviews = reviewArray.map((review) => {
+                return `<li>${review.content}</li>`
+              })
+              let reviewsGrouped = reviews.join('')
+              let group = `<div><img src="https://image.tmdb.org/t/p/w500/${result.poster_path}"style="height: 250; width: 200"/><li>${result.title}</li>${reviewsGrouped}`
+              moviesUL.innerHTML += group
             }
-          })
           })
       })
     })
+})
