47093d4fb051276c554483bdf3d34d818557d4bc
Collin Barlow <Collin@Collins-MacBook-Pro.local>
2019-12-13 13:57:46 -0600

    ready to deploy

--- reviews.js
+++ reviews.js
@@ -26,36 +26,37 @@ movieSearch.addEventListener('click', () => {
             })
 
             let reviewsGrouped = reviews.join('')
-            let group = `
-            <div class="media">
-            <img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" class="align-self-auto mr-3" alt="noImage" onerror="this.src='timages/noImage.jpg'" style="hieght: 300; width: 200;"/>
-            <div class="media-body">
-            <h1 class="mt-0" style="padding-top: 10;">${result.title}</h1>
-            <p>${reviewsGrouped}</p>
-            <div id="${id}div">
-            </div>
-            </div>
-            </div>`
+            let group = `<div class="media">
+                          <img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" class="align-self-auto mr-3" alt="noImage" onerror="this.src='timages/noImage.jpg'" style="hieght: 300; width: 200;"/>
+                          <div class="media-body">
+                            <h1 class="mt-0" style="padding-top: 10;">${result.title}</h1>
+                            <p>${reviewsGrouped}</p>
+                            <div id="${id}div">
+                            </div>
+                          </div>
+                        </div>`
+
             let submitReviews = `<div class="form-group mx-sm-3 mb-2" style="display: flex; align-items: baseline; margin: 10 0;">
-            <div style="width: 275;">Submit Review or Edit Review</div>
-            <input type="text" class="form-control" id='${id}' placeholder="Enter Review" style="margin: 0 10;">
-            <button id="movieSearch" onClick='addReview(${id}, "${title}")' type="submit" class="btn btn-primary mb-2">
-            Submit
-            </button>
-            </div>`
+                                  <div style="width: 275;">Submit Review or Edit Review</div>
+                                  <input type="text" class="form-control" id='${id}' placeholder="Enter Review" style="margin: 0 10;">
+                                  <button id="movieSearch" onClick='addReview(${id}, "${title}")' type="submit" class="btn btn-primary mb-2">
+                                    Submit
+                                  </button>
+                                </div>`
 
             db.collection(`${id}`).get()
               .then(function(querySnapshot) {
                 querySnapshot.forEach(function(doc) {
                   let grab = document.getElementById(`${id}div`)
                   let review = doc.data();
-                  let reviewContent = `<h5 class="mt-0">${review.username}</h5><p class="reviewText">${review.review}</p>`
+                  let reviewContent = `<h5 class="mt-0">${review.username}</h5>
+                                       <p class="reviewText">${review.review}</p>`
                   grab.innerHTML += reviewContent
                 });
               })
             moviesUL.innerHTML += group
             moviesUL.innerHTML += submitReviews
-        });
+          });
       })
     })
 })
@@ -69,9 +70,17 @@ function addReview(id, title) {
       title: title,
       review: movieReview,
     })
-    let grab = document.getElementById(`${id}div`)
-    let reviewContent = `<h5 class="mt-0">${userNow}</h5><p class="reviewText">${movieReview}</p>`
-    grab.innerHTML += reviewContent
+    db.collection(`${id}`).get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          let grab = document.getElementById(`${id}div`)
+          grab.innerHTML = ''
+          let review = doc.data();
+          let reviewContent = `<h5 class="mt-0">${review.username}</h5>
+                               <p class="reviewText">${review.review}</p>`
+          grab.innerHTML += reviewContent
+        });
+      })
   }
 }
 
@@ -79,60 +88,60 @@ function linkedFromOtherPage() {
   let movieTitle = location.hash.substring(1);
   let movieid = movieTitle;
   moviesUL.innerHTML = ''
-if(movieid != '') {
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&query=${movieid}&page=1&include_adult=false`)
-    .then((response) => {
-      return response.json()
-    })
-    .then(json => {
-      let results = json.results
-      let nowplaying = results.map((result) => {
-        let id = result.id
-        let title = result.title
-
-        fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&page=1`)
-          .then((response) => {
-            return response.json()
-          })
-          .then(json => {
-            let reviewArray = json.results
-            let reviews = reviewArray.map((review) => {
-              return `<h5 class="mt-0">${review.author}</h5><p class="reviewText">${review.content}</p>`
-            })
-
-            let reviewsGrouped = reviews.join('')
-            let group = `
-            <div class="media">
-            <img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" class="align-self-auto mr-3" alt="noImage" onerror="this.src='timages/noImage.jpg'" style="hieght: 300; width: 200;"/>
-            <div class="media-body">
-            <h1 id="padding10" class="mt-0">${result.title}</h1>
-            <p>${reviewsGrouped}</p>
-            <div id="${id}div">
-            </div>
-            </div>
-            </div>`
-            let submitReviews = `<div class="form-group mx-sm-3 mb-2" style="display: flex; align-items: baseline; margin: 10 0;">
-            <div style="width: 120;">Submit Review</div>
-            <input type="text" class="form-control" id='${id}' placeholder="Enter Review" style="margin: 0 10;">
-            <button id="movieSearch" onClick='addReview(${id}, "${title}")' type="submit" class="btn btn-primary mb-2">
-            Submit
-            </button>
-            </div>`
-
-            db.collection(`${id}`).get()
-              .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                  let grab = document.getElementById(`${id}div`)
-                  let review = doc.data();
-                  let reviewContent = `<h5 class="mt-0">${review.username}</h5><p class="reviewText">${review.review}</p>`
-                  grab.innerHTML += reviewContent
-                });
-              })
-            moviesUL.innerHTML += group
-            moviesUL.innerHTML += submitReviews
-        });
+  if(movieid != '') {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&query=${movieid}&page=1&include_adult=false`)
+      .then((response) => {
+        return response.json()
       })
-    })
-}
+      .then(json => {
+        let results = json.results
+        let nowplaying = results.map((result) => {
+          let id = result.id
+          let title = result.title
+
+          fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&page=1`)
+            .then((response) => {
+              return response.json()
+            })
+            .then(json => {
+              let reviewArray = json.results
+              let reviews = reviewArray.map((review) => {
+                return `<h5 class="mt-0">${review.author}</h5>
+                        <p class="reviewText">${review.content}</p>`
+              })
+              let reviewsGrouped = reviews.join('')
+              let group = `<div class="media">
+                            <img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" class="align-self-auto mr-3" alt="noImage" onerror="this.src='timages/noImage.jpg'" style="hieght: 300; width: 200;"/>
+                            <div class="media-body">
+                              <h1 id="padding10" class="mt-0">${result.title}</h1>
+                              <p>${reviewsGrouped}</p>
+                              <div id="${id}div">
+                              </div>
+                            </div>
+                           </div>`
+              let submitReviews = `<div class="form-group mx-sm-3 mb-2" style="display: flex; align-items: baseline; margin: 10 0;">
+                                    <div style="width: 120;">Submit Review</div>
+                                    <input type="text" class="form-control" id='${id}' placeholder="Enter Review" style="margin: 0 10;">
+                                    <button id="movieSearch" onClick='addReview(${id}, "${title}")' type="submit" class="btn btn-primary mb-2">
+                                      Submit
+                                    </button>
+                                   </div>`
+
+              db.collection(`${id}`).get()
+                .then(function(querySnapshot) {
+                  querySnapshot.forEach(function(doc) {
+                    let grab = document.getElementById(`${id}div`)
+                    let review = doc.data();
+                    let reviewContent = `<h5 class="mt-0">${review.username}</h5>
+                                         <p class="reviewText">${review.review}</p>`
+                    grab.innerHTML += reviewContent
+                  });
+                })
+                moviesUL.innerHTML += group
+                moviesUL.innerHTML += submitReviews
+            });
+        })
+      })
+  }
 }
 linkedFromOtherPage()
--- upcomingmovies.js
+++ upcomingmovies.js
@@ -1,59 +1,36 @@
 fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=ddcb97784f13102b72af6aa3e89493b4&language=en-US&page=1&region=US")
-.then((response) => {
-  return response.json()
-})
-.then(json => {
-  let results = json.results
-  let upcoming = results.map((result) => {
-    let poster = result.poster_path
-    return `<a id="margin25" class="tile" href="reviews.html#${result.title}">
-              <div class="tile__media">
-                <img class="tile__img_nowplaying" src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="No Image" onerror="this.src='timages/noImage.jpg'">
-              </div>
-              <div class="tile__details">
-                <div class="tile__title">
-                ${result.title} Release Date: ${result.release_date}
-                </div>
-              </div>
-            </a>`
+  .then((response) => {
+    return response.json()
   })
-  for(let index = 0; index < 5; index++) {
-    let movie = upcoming[index]
-    zeroThroughFive.innerHTML += movie
-  }
-  for(let index = 5; index < 10; index++) {
-    let movie = upcoming[index]
-    fiveThroughTen.innerHTML += movie
-  }
-  for(let index = 10; index < 15; index++) {
-    let movie = upcoming[index]
-    tenThroughFifteen.innerHTML += movie
-  }
-  for(let index = 15; index < 20; index++) {
-    let movie = upcoming[index]
-    fifteenThroughTwenty.innerHTML += movie
-  }
-})
-
-
-
-
-
-
-
-
-
-
-
-/*let results = json.results
-let nowplaying = results.map((result) => {
-  return `<a href="reviews.html#${result.title}" class="linkReview">
-  <img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="No Image" onerror="this.src='timages/noImage.jpg'" style="height: 250; width: 200"/>
-  <div style="text-align: center; max-width: 200;">
-  <li style="list-style-type: none;">${result.title}</li>
-  <li style="list-style-type: none;">Release Date:${result.release_date}</li>
-  </div>
-  </a>`
-})
-moviesUL.innerHTML = nowplaying.join('')
-})*/
+  .then(json => {
+    let results = json.results
+    let upcoming = results.map((result) => {
+      let poster = result.poster_path
+      return `<a id="margin25" class="tile" href="reviews.html#${result.title}">
+                <div class="tile__media">
+                  <img class="tile__img_nowplaying" src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="No Image" onerror="this.src='timages/noImage.jpg'">
+                </div>
+                <div class="tile__details">
+                  <div class="tile__title">
+                    ${result.title} Release Date: ${result.release_date}
+                  </div>
+                </div>
+              </a>`
+    })
+    for(let index = 0; index < 5; index++) {
+      let movie = upcoming[index]
+      zeroThroughFive.innerHTML += movie
+    }
+    for(let index = 5; index < 10; index++) {
+      let movie = upcoming[index]
+      fiveThroughTen.innerHTML += movie
+    }
+    for(let index = 10; index < 15; index++) {
+      let movie = upcoming[index]
+      tenThroughFifteen.innerHTML += movie
+    }
+    for(let index = 15; index < 20; index++) {
+      let movie = upcoming[index]
+      fifteenThroughTwenty.innerHTML += movie
+    }
+  })
